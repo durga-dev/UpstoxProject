a3fdbe3f96c023bd39373cb2deb73a609f0bd9f6
Durga <panigrahi.durga9@gmail.com>
2022-12-01 20:16:48 +0530

    Rest file added

--- UpStocksProjectTests/UpStocksProjectTests.swift
+++ UpStocksProjectTests/UpStocksProjectTests.swift
@@ -6,30 +6,47 @@
 //
 
 import XCTest
+@testable import UpStocksProject
 
 final class UpStocksProjectTests: XCTestCase {
 
+    var homeViewModel: HomeViewModel?
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        homeViewModel = HomeViewModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        homeViewModel = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testSumWithSuccess() {
+        let sum = homeViewModel?.sum(a: 5, b: 3)
+        XCTAssertTrue(sum == 8)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func testSumWithFail() {
+        let sum = homeViewModel?.sum(a: 5, b: 3)
+        XCTAssertFalse(sum == 7, "hhhhh")
+    }
+    
+    func testViewDidLoadSuccess() {
+        let loadingExpectation = expectation(description: "Wait for loading data")
+        homeViewModel?.updateHoldingData = { holdingData in
+            XCTAssert(
+                holdingData != nil,
+                "Data not loaded"
+            )
+            loadingExpectation.fulfill()
         }
+        
+        homeViewModel?.viewDidLoad()
+        
+        wait(
+            for: [
+                loadingExpectation
+        ],
+            timeout: 2, enforceOrder: true)
     }
 
 }
